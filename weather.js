053593f2c89028c10e52190fd4c91185caad2cca
jgBayo <jordigbayo@gmail.com>
2018-04-29 11:16:45 +0200

    Version with button-needs to get around the api loading time.

--- weather.js
+++ weather.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
-    $("#test-button").on("click", function() {
-        $("#test-button").hide();
+    var lat = "";
+    var lon = "";
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        lat=position.coords.latitude;
+        lon=position.coords.longitude;
+      });
+      
+      $("#tButton").on("click", function() {
+      
+      $.ajax({
+        type: "GET",
+        //url:"https://fcc-weather-api.glitch.me/api/",
+        url: "https://fcc-weather-api.glitch.me/api/current?lon="+lon+"&lat="+lat,
+        dataType: "json",
+        success: function(result) {
+            console.log(result);
+            $(".location").html("Location: " + result.name);
+            $(".temperature").html("Temperature: "+ result.main.temp+ " ºC");
+            $(".pressure").html("Pressure: "+ result.main.pressure + " bar");
+            $(".weather").html(result.weather[0].main);
+            $(".icon").html("<img src="+result.weather[0].icon+" alt=\"current weather icon\">");
+        }
     });
-});
+      });
+    });
